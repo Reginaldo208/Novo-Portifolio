51f205eb1ab8a96402f1d79cb44bc8fa16ec7d15
Reginaldo208 <100892648+Reginaldo208@users.noreply.github.com>
2023-10-17 00:04:57 -0300

    main

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
+let lastScrollTop = 0;
+const header = document.querySelector('header');
 
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Rolar para baixo
+        header.style.top = '-100px'; // Altere para a altura do seu cabeçalho
+    } else {
+        // Rolar para cima
+        header.style.top = '0';
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
